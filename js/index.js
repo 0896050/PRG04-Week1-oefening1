345c4528710ba7e0cbfc4f3800ddd98d371c7a08
rick pijbes <rick pijbes>
2017-05-15 14:12:41 +0200

    Opdracht 1 klaar

--- js/index.js
+++ js/index.js
@@ -1,29 +1,40 @@
 // start when the document is loaded
 console.log("starting...");
+let wordsStartArray = [];
+let wordsEndArray = [];
+window.addEventListener("load", init);
 
-function init() {
+function init() 
+{
   // create data
   let wordsStart = "Dum,Dam,Da,Dum,Da,Da,Da,Dum,Thats,The,Way,You've,Got,To,GO!";
-  let wordsEnd = "No,No,No,No,No,No,No,No,No,No,No,No,There's,No,Limit";  
+  let wordsEnd = "No,No,No,No,No,No,No,No,No,No,No,No,There's,No,Limit";
+  wordsStartArray = wordsStart.split(",");
+  wordsEndArray = wordsEnd.split(",");
 
   buildButtons();
 }
 
 // build the row of buttons. each button has one label from the list of words
-function buildButtons() {
-  for (let i = 0; i < wordsStart.length; i++) {
-    let button = createButton();
+function buildButtons() 
+{
+  for (let i = 0; i < wordsStartArray.length; i++) 
+  {
+    let button = createButton(wordsStartArray[i], wordsEndArray[i]);
   }
 }
 
 // build one button that changes its label when clicked
-function createButton() {
+function createButton(startWord, endWord) 
+{
   let button = document.createElement("button");
-  button.innerHTML = "start word here";
-  button.endText = "end word here";
+  button.innerHTML = startWord;
+  button.endText = endWord;
 
-  button.addEventListener("click", function(e) {
-    e.target.innerHTML = "new word here";
+  button.addEventListener("click", function(e)
+  {
+    console.log('Deze werkt')
+    e.target.innerHTML = e.target.endText;
   });
   
   document.body.appendChild(button);
